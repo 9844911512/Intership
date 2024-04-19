dff0cff8dbdb4cd49c53ba89e3df6ae9bef76f5b
9844911512 <41988082+9844911512@users.noreply.github.com>
2024-04-19 22:22:40 +0530

    Add files via upload

---
+++ Test 1.0 .py
@@ -0,0 +1,486 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Test 1.0 Batch 2(AI/ML)
+# 
+# ## All the Best
+
+# # MCQ 
+# 
+# ## Any 15
+
+# ## Question 1 
+# ![image.png](attachment:a628199e-1105-434b-8234-25842aa1038f.png)
+A. ['July', 'September', 'December']
+
+B. July
+
+C. September
+
+D. December                                   
+# In[ ]:
+
+
+Ans = C
+
+
+# ## Question 2
+# ![image.png](attachment:c60cafc7-0c19-49f1-9ddb-7dcc2dc1fc91.png)
+# 
+# 
+A. List
+
+B. String
+
+C. Dictionary
+
+D. TupleAns =A
+# ## Question 3
+# 
+# ![image.png](attachment:642bf602-83be-4a65-8530-13c3a6641e4f.png)
+# 
+# 
+A. m and n
+
+B. False
+
+C. True
+
+D. mn
+# In[ ]:
+
+
+Ans = D
+
+
+# ## Question 4
+# 
+# ![image.png](attachment:106fdc49-86a5-47ee-b6b3-cde498e7a013.png)
+
+# In[ ]:
+
+
+A. PlayPlay
+
+B. Play
+
+B. Play2
+
+D. Play*2
+
+
+# In[ ]:
+
+
+D
+
+
+# ## Question 5
+# 
+# ![image.png](attachment:788b8c29-4f83-4631-bc66-87286ab8868b.png)
+# 
+# 
+A. <NA>
+
+B. False
+
+C. True
+
+D. Error
+# In[ ]:
+
+
+C
+
+
+# In[ ]:
+
+
+Ans= 
+
+
+# ## Question 6
+# 
+# ![image.png](attachment:fc9684b9-fc36-4bd5-8d94-60a74b6800ee.png)
+
+# A. [3, 6, 9, 12, 15]
+# 
+# B. [0, 3, 6, 9, 12]
+# 
+# C. [0, 3, 6, 9, 12, 15]
+# 
+# D. []
+
+# In[ ]:
+
+
+ANS = A
+
+
+# ## Question 7
+# 
+# ![image.png](attachment:a8c781f2-7f68-4e50-83c0-ec1585fec50d.png)
+A. CC
+
+B. DD
+
+C. BB
+
+D. Error
+# In[ ]:
+
+
+ANS = D
+
+
+# ## Question 8
+# 
+# ![image.png](attachment:1a0b73dd-9623-430a-ac34-7faeafe92863.png)
+# 
+A. 10
+
+B. 20
+
+C. x
+
+D. y
+# In[ ]:
+
+
+ANS = 
+
+
+# ## Question 9
+# 
+# ![image.png](attachment:1e5de376-a95c-42c1-962f-5f8f4aa92bf3.png)
+A. Series.to_frame()
+
+B. Series.to_dataframe()
+
+C. Series.frame()
+
+D. None of the above
+# In[ ]:
+
+
+ANS = B
+
+
+# ## Question 10
+# 
+# ![image.png](attachment:8bdf2b75-68ba-41ac-b54b-af9e951c2b04.png)
+# 
+# 
+A. asarray()
+
+B. to_numpy()
+
+C. to_array()
+
+D. None
+# In[ ]:
+
+
+ANS = A
+
+
+# ## Question 11
+# ![image.png](attachment:a57a6125-02b0-4f1a-9fff-c9b885e36630.png)
+A.Integers
+
+B. Built-in functions
+
+C. Strings
+
+D. All of the above
+# In[ ]:
+
+
+ANS = A
+
+
+# # Question 12
+# 
+# ![image.png](attachment:bf0a49f0-3802-43dd-a21c-c30d429f5dd0.png)
+A. 6
+
+B. 18
+
+C. 36
+
+D. 216
+# In[ ]:
+
+
+ANS = B
+
+
+# # Question 13
+# 
+# ![image.png](attachment:31485b9c-de51-42a6-8edd-4bd4c2eebfb5.png)
+A. Keyframe
+
+B. Dataframe
+
+C. Series
+
+D. None
+# In[ ]:
+
+
+Ans= C
+
+
+# # Question 14
+# 
+# ![image.png](attachment:4a90ab21-9003-4151-bae3-fb4016ba341a.png)
+A. addition
+
+B. subtraction
+
+C. multiplication
+
+D. division
+# In[ ]:
+
+
+ANS= C and D
+
+
+# # Question 15
+# 
+# ![image.png](attachment:acb7cb76-9a27-4a36-aebd-d26b3122048f.png)
+# 
+# 
+A. plt.plot(xpoints, ypoints)
+
+B. plt.plot(xpoints, ypoints, 0)
+
+C. plt.plot(xpoints, ypoints, False)
+
+D. plt.plot(xpoints, ypoints, 'o')
+# In[ ]:
+
+
+ANS = C
+
+
+# # Question 16
+# ![image.png](attachment:0235b451-a4b8-4e94-85bc-7269054a0aaf.png)
+# 
+# 
+
+# A. ylabel()
+# 
+# B. set_y()
+# 
+# C. y-axis()
+# 
+# D. yaxis()
+
+# In[ ]:
+
+
+ANS = A
+
+
+# # Question 17
+# 
+# ![image.png](attachment:64e61ae5-9ea5-4870-9361-50dd01be15cf.png)
+A. argmax() and argmin()
+
+B. max() and min()
+
+C. amax() and amin()
+
+D. None of the above
+# In[ ]:
+
+
+Ans =B
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # Python Basic Exercise 
+# 
+
+# ### Question 1 - 
+# ![image.png](attachment:image.png)
+# 
+
+# In[10]:
+
+
+def Pairs(ls, p): 
+    res = []
+    while ls:
+        num = ls.pop()
+        diff = p - num
+        if diff in ls:
+            res.append((diff, num))
+         
+    res.reverse()
+    return res
+     
+ls = [1, 5, 3, 7, 9]
+p= 12
+print(Pairs(ls, p))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Question 2 - 
+# 
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+Ans =
+
+
+# In[ ]:
+
+
+
+
+
+# ### Question 3 - 
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+Ans = for i in range(n)
+         print(i+n)
+
+
+# In[ ]:
+
+
+
+
+
+# # Theory Questions
+# 
+# 
+
+# ### Question 1
+# 
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+Ans= confusion matrix represents performance of the model.
+It displays the number of true positives, true negatives, false positives, and false negatives.
+
+
+# In[ ]:
+
+
+
+
+
+# ### Question 2
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+Ans= numpy.linspace() function create evenly spaced arrays with a specified number of elements.
+
+
+# In[5]:
+
+
+import numpy as np
+np.linspace(0,2,10)
+
+
+# ### Question 3
+# 
+# 
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+#overfitting: When a model gets trained with so much data,it can't predict accurate result with testing data.In otherwords,model learns the dats too much.
+
+
+# In[ ]:
+
+
+#underfitting:models learns the data very low.there models works well with training data but not with tesing data.
+
+
+# # Mathematics 
+
+# ### Question 1: Explain how entropy and Gini are calculated by a decision tree.
+# 
+# ![image.png](attachment:eca3be63-df5c-46ec-9676-676c607c86ec.png)
+# 
+# From the above dataset , you have to calculate entropy and Gini for *humidity*
+
+# In[ ]:
+
+
+Ans=gini index(outlook=sunny)=1-(2/5)^2-(3/5)^2
+gini index(outlook=overcast)=1-(4/4)^2-(0/5)^2
+gini index(outlook=rainfall)=1-(3/5)^2-(2/5)^2
+
+
+# In[ ]:
+
+
+entropy
+
+
+# #### Question 2:
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+Ans=some datas are out of boundaries from the rest of the data points.these are called outliers.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#Submit the answer via Github
+
